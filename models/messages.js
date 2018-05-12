6308addeec3ddb4c074b549949306f98db3184e2
Apollo <jackson.harris3@gmail.com>
2018-05-12 12:10:58 -0700

    init messages

---
+++ models/messages.js
@@ -0,0 +1,37 @@
+module.exports = function(sequelize, DataTypes) {
+    var Messages = sequelize.define(
+        'Messages',
+        {
+            message: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            isAccepted: {
+                type: DataTypes.boolean,
+                defaltValue: false
+            }
+        },
+        {
+            freezeTableName: true,
+        }
+    );
+
+    Messages.associate = function(models) {
+        Messages.hasOne(models.Renters, {
+            foreignKey: {
+                allowNull: false,
+            }
+        });
+    };
+
+    Messages.associate = function (models) {
+        Messages.hasOne(models.Lenders, {
+            foreignKey: {
+                allowNull: false,
+            }
+        })    
+    };
+
+    return Messages;
+
+};
