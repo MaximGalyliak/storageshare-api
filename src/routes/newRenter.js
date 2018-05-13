2d6693afb350675a69444cd1280e851287bb59c7
Apollo <jackson.harris3@gmail.com>
2018-05-13 01:01:34 -0700

    add post request handler

---
+++ src/routes/newRenter.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../models')
+
+// Create new Renter
+router.get('/', function (req, res) {
+    res.send('form',{title: 'this will be the newrenter form api'})
+})
+
+router.post('/api/newrenter', (req, res,) => {
+    db.Renters.create({
+        name: req.body.name,
+        email: req.body.email,
+        paypal_id: req.body.paypal,
+        phone: req.body.phone,
+        address: req.body.address,
+    }).then(response => {
+        res.status(201).json({newRenterId: response.dataValues.id});
+    }).catch( error => {
+        res.sendStatus(500);
+    });
+});
+
+module.exports = router;
