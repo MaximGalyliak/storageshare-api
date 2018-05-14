24004b7709e33e3f8410dd9eaf5776e4eef61941
Apollo <jackson.harris3@gmail.com>
2018-05-13 18:51:30 -0700

    prep for rebase

--- src/routes/newRenter.js
+++ src/routes/newRenter.js
@@ -8,15 +8,15 @@ router.get('/', function (req, res) {
     res.send('form',{title: 'this will be the newrenter form api'})
 })
 
-router.post('/api/newrenter', (req, res,) => {
-    db.Renters.create({
-        name: req.body.name,
-        email: req.body.email,
-        paypal_id: req.body.paypal,
-        phone: req.body.phone,
-        address: req.body.address,
-    }).then(response => {
-        res.status(201).json({newRenterId: response.dataValues.id});
+router.post('/api/newrenter/signup', (req, res,) => {
+    db.Renters.create(req.body
+        // name: req.body.name,
+        // email: req.body.email,
+        // paypal_id: req.body.paypal,
+        // phone: req.body.phone,
+        // address: req.body.address,
+    ).then(response => {
+        res.status(201).render({newRenterId: response.dataValues.id});
     }).catch( error => {
         res.sendStatus(500);
     });
