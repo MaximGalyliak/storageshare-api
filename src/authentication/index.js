5c3990ed94a3629612778b1bb4d6f6037afef2c1
madhavparekh <madhavparekh@gmail.com>
2018-05-15 12:21:34 -0700

    now handles mutiple local strategy (ie, renters or lenders)

--- src/authentication/index.js
+++ src/authentication/index.js
@@ -1,33 +1,68 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-var { Renters } = require('../models');
+var db = require('../models');
+var table;
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-  },
-  function(email, password, done) {
-    Renters.findOne( { where: { email: email } })
-    .then(renter => {
-      if(!renter) {
-        return done(null, false, { message: 'Incorrect email' });
-      }
-      if(!renter.validPassword(password))
-        return done(null, false, { message: "Incorrect password" });
-      return done(null, renter);
-    })
-      .catch(err => done(err) )
-  }
-));
+passport.use(
+	'renters',
+	new LocalStrategy(
+		{
+			usernameField: 'email',
+			passwordField: 'password',
+		},
+		function(email, password, done) {
+			db.Renters.findOne({ where: { email: email } })
+				.then((renter) => {
+					if (!renter) {
+						return done(null, false, { message: 'Incorrect email' });
+					}
+					if (!renter.validPassword(password))
+						return done(null, false, { message: 'Incorrect password' });
+					return done(null, renter);
+				})
+				.catch((err) => done(err));
+		}
+	)
+);
+
+passport.use(
+	'lenders',
+	new LocalStrategy(
+		{
+			usernameField: 'email',
+			passwordField: 'password',
+		},
+		function(email, password, done) {
+			db.Lenders.findOne({ where: { email: email } })
+				.then((lender) => {
+					if (!lender) {
+						return done(null, false, { message: 'Incorrect email' });
+					}
+					if (!lender.validPassword(password))
+						return done(null, false, { message: 'Incorrect password' });
+					return done(null, lender);
+				})
+				.catch((err) => done(err));
+		}
+	)
+);
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-  Renters.findById(id)
-    .then(renter => done(null, renter.dataValues))
+	console.log(id);
+
+	db.Lenders.findById(id)
+		.then((lender) => done(null, lender.dataValues))
+		.catch((err) => {
+			if (err) {
+				console.log('renters');
+				db.Renters.findById(id).then((renter) => done(null, renter.dataValues));
+			}
+		});
 });
 
 module.exports = passport;
