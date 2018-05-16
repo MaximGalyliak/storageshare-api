e9faef87a8afd1daf2f85b5c05f7a5c17b4674b5
Apollo <jackson.harris3@gmail.com>
2018-05-16 01:51:36 -0700

    finish api request formatting

--- src/config/maps.js
+++ src/config/maps.js
@@ -7,8 +7,9 @@ const baseURL = 'https://maps.googleapis.com/maps/api/distancematrix/json?origin
 function getDistances(renterAddress, locationAdresses) {
     let origin = renterAddress.replace(/,/g, '').replace(/ /g , '+')
     let destinations = locationAdresses.map(e => e + '|').toString().replace(/,/g , '').replace(/ /g , '+')
-    let distanceMatrixRequest = baseURL + origin + '&destinations=' + destinations
-    console.log(destinations.substring(0, destinations.length - 1))
+    let distanceMatrixRequest = baseURL + origin + '&destinations=' + destinations.substring(0, destinations.length - 1) + '&key=' +key.access_token_key;
+    console.log(distanceMatrixRequest)
+    // console.log(destinations.substring(0, destinations.length - 1))
 
 }
 
