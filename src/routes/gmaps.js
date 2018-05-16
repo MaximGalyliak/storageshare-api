488096ae452ecc29cd26bfb6e24d8312494672b5
Apollo <jackson.harris3@gmail.com>
2018-05-15 21:40:14 -0700

    changing api request to use locations address instead of the lenders address

--- src/routes/gmaps.js
+++ src/routes/gmaps.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 
-var { Lenders, Renters } = require('../models');
+var { Lenders, Renters, Locations } = require('../models');
 var db = require('../models');
 
 /* GET home page. */
@@ -20,17 +20,61 @@ router.get('/lenders', (req,res) => {
 })
 
 router.get('/renter/:user', (req, res) => {
-    db.Renters.findAll({
-        where: {
-            id: req.params.user  
-        }
+    db.Renters.findById(req.params.user,
+    { 
+        attributes: ['address']  
     }).then((data) => {
         console.log(data);
     });  
 });
 
-router.get('/distanceCalc', (req, res) => {
+router.get('/findspace/:user', (req, res) => {
+    
+    let userID = req.params.user;
+    let allLenders = null;
+    let currentRenter = null;
+    let allLocations = null;
 
+    //pulls the current renter/user data from the db
+    function getRenter() {
+        db.Renters.findById(req.params.user,
+        { 
+        attributes: ['address']  
+        }).then((data) => {
+            console.log(data);
+        });
+    }
+
+    //pulls the locations from the db
+    function getLocations() {
+        db.Locations.findAll({
+            attributes: ['LenderId', 'address']
+        })
+    }
+
+    //pulls all lenders from the db
+    function getLenders() {
+        db.Lenders.findAll({})
+            .then((response) => {
+                console.log(response)
+                allLenders = response
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }        
+    getLocations()
+
+    //uses bluebird to properly chain the db requests and storethe data for the gmaps api request
+    process.on("promiseChained", function(getLocations, getRenter){
+        allLocations = event.details.promise;
+        currentRenter = event.details.child;
+        console.log(allLocations)    
+        console.log(currentRenter)    
+    });
+        
 })
+
+
 
 module.exports = router;
